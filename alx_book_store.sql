28bbe639a0e50db07372deee8094fac215580ddd
fred <86912882+fredDecodes@users.noreply.github.com>
2025-10-19 13:48:24 +0100

    Add script to create a database and objects

---
+++ alx_book_store.sql
@@ -0,0 +1,41 @@
+-- A Magical Database for Your Dream Online Bookstore Adventure!
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+-- Books: Stores information about books available in the bookstore.
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT FOREIGN KEY REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+-- Authors: Stores information about authors.
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215),
+);
+
+-- Customers: Stores information about customers.
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
+);
+
+-- Orders: Stores information about orders placed by customers.
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
+    order_date DATE,
+);
+
+-- Order_Details: Stores information about the books included in each order.
+CREATE TABLE Order_Details (
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT FOREIGN KEY REFERENCES Orders(order_id),
+    book_id INT FOREIGN KEY REFERENCES Books(book_id),
+    quantity DOUBLE,
+);
