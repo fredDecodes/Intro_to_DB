1d95243bb42e88fede4dce1e66b620931b090d05
fred <86912882+fredDecodes@users.noreply.github.com>
2025-10-26 23:12:40 +0000

    Insert record into customer table

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- Populating Your Database with Your Very First Data
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Inserting data into the books table
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
