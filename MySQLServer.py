bc9754e09d6ee733388bb29ba84b15d8c5f74503
fred <86912882+fredDecodes@users.noreply.github.com>
2025-10-26 22:10:09 +0000

    Update error handling code

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,6 +17,6 @@ my_cursor = my_db.cursor()
 try:
     my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error creating database: {e}")
 
